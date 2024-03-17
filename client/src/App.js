3158cbcab75adb44efe2dda7626323e153c84ec2
mausooq <mauchoomausooq051@gmail.com>
2024-03-17 21:40:58 +0530

    src frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,18 @@
+import { AppBar, Container, Typography } from '@material-ui/core';
 import React from 'react';
+
+import memories from './images/memories.png'
 
 const App = () =>{
     return(
-        <div>
-            <h1>
-                APP
-            </h1>
-        </div>
+      <Container maxWidth="lg">
+        <AppBar position='static' color='inherit'>
+        <Typography variant='h4' align='center'>
+               NOSTALGIA NET
+        </Typography>
+        <img src={memories} alt='Memories' height="60"/>
+        </AppBar>
+      </Container>
     )
 }
 
