9ede24f4cc98e3e4a37698ee4a86d20e7fe29123
mausooq <mauchoomausooq051@gmail.com>
2024-04-08 22:05:03 +0530

    new

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore,applyMiddleware,compose } from redux;
-import thunk from "redux-thunk";
-import reducers from "reducers";
+import { createStore,applyMiddleware,compose } from 'redux';
+import thunk from 'redux-thunk';
+import reducers from "./reducers";
 
 
 import App from './App';
